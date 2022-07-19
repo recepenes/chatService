eee2494637e0912e44431b3e2a2a6dc72e5d9ff6
recepenes <turktenacious@gmail.com>
2022-07-19 19:25:16 +0300

    Add NUnit test project

---
+++ client/Program.cs
@@ -0,0 +1,25 @@
+﻿using client.Services;
+
+namespace client
+{
+    class Program
+    {
+        public static IClientService _clientService { get; set; }
+        static void Main(string[] args)
+        {
+            var client = new ClientService(100);
+
+            _clientService = client;
+
+            Console.Title = $"The Client #{_clientService.ID}";
+
+            Console.WriteLine($"{_clientService.ID} trying to connect...");
+            _clientService.Connect();
+            Console.WriteLine($"{_clientService.ID} CONNECTED.");
+
+            Console.WriteLine($"{_clientService.ID} started to listening...");
+            _clientService.Listen();
+
+        }
+    }
+}
