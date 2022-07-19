b2b14749f1301d70ee5dfd38463de49b8b676ec6
recepenes <turktenacious@gmail.com>
2022-07-19 18:54:35 +0300

    Add ClientModel Add ServerService

---
+++ server/Models/ClientModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Sockets;
+
+namespace server.Models
+{
+    public class ClientModel
+    {
+        public Socket Socket { get; set; }
+        public bool isSended { get; set; }
+    }
+}
